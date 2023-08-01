358de18bc194a16c93e4b44e7dd5a59a44f6c9e1
Bruno Chagas <139476631+Brunochdev@users.noreply.github.com>
2023-08-01 20:34:57 -0300

    Primeiro Programa em Java
    Introdução as Classes, Pacotes e demais estruturas dentro de Java

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+public class PrimeiroPrograma {  //classe
+    public static void main(String[] args) { //método (main), "psvm" escreve todo o comando do método
+       System.out.println("Ola, Mundo!"); //saída na tela, "sout" escreve todo o comando de saída 
+    }                                       //"println" imprime a mensagem e pula uma linha 
+}
+
+
+/*Ou seja, tem um método principal (main) dentro de uma classe (PrimeiroPrograma) que faz parte do 
+pacote (primeiroprograma).
+Classes são compostas por métodos, e um pacote pode ter várias classes*/
+
+/* BrunoChagas -> Primeira letra maiúscula vai ser classe ou interface
+ * brunoChagas -> Primeira letra minúscula e o resto como camelcase pode ser:
+                                                            -atributo
+                                                            -variável
+                                                            -método
+ * brunochagas -> Todas as letras minúsculas indica um pacote                                                            
+ * BRUNOCHAGAS -> Todas as letras maiúsculas indica uma constante
+ */
